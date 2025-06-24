36567c143dc3323854e9e9b8e5b7b69703bb8dc7
Krzysztof Grabski <krzysztof.grabski@oxla.com>
2025-06-24 14:38:42 +0200

    Minor flags.

--- prepare_and_upload_oxla_image.sh
+++ prepare_and_upload_oxla_image.sh
@@ -4,7 +4,8 @@ set -xe
 
 source ~/.oxla/compile
 
-docker build -t ${SQLANCER_IMAGE_TAG} -f oxla.Dockerfile .
+docker build -t ${SQLANCER_IMAGE_TAG} -f oxla.Dockerfile --pull --progress=plain .
+
 docker push ${SQLANCER_IMAGE_TAG}
 
 echo -e "Image built and pushed to Harbor. Tag: ${SQLANCER_IMAGE_TAG}"
